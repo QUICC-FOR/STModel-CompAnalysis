1c985d5ddc0cef631ec428dc9463a7f2589d3090
SteveViss <s.vissault@yahoo.fr>
2016-02-16 14:08:44 -0500

    enhancements submission script

---
+++ submit_turnover.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -N STM-turnover
+#PBS -l walltime=24:00:00
+#PBS -l nodes=4:ppn=1
+#PBS -q qwork@mp2
+#PBS -j oe
+
+module load bioinformatics/R/3.2.3
+
+cd $PBS_O_WORKDIR
+RScript ./Turnover.r
