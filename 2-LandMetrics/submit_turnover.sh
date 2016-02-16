6cb3e45874233666a226a535684d5093b36daae4
SteveViss <s.vissault@yahoo.fr>
2016-02-16 15:02:25 -0500

    Fix submit

--- 2-LandMetrics/submit_turnover.sh
+++ 2-LandMetrics/submit_turnover.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #PBS -N STM-turnover
 #PBS -l walltime=12:00:00
-#PBS -l nodes=4:ppn=1
-#PBS -q qwork@mp2
+#PBS -l nodes=12:ppn=1
+#PBS -q qfbb
 #PBS -j oe
 
 module load bioinformatics/R/3.2.3
+module load gcc/5.2.0
+module load gdal/1.11.1
 
 cd $PBS_O_WORKDIR
-RScript ./Turnover.r
+Rscript ./Turnover.r
