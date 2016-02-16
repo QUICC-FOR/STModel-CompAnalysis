65dc9ded619e1ee709a3cc58bf4e9e8a6a1cec6b
SteveViss <s.vissault@yahoo.fr>
2016-02-16 13:30:18 -0500

    Init repo

---
+++ 1-Transitions/glm_transitions.r
@@ -0,0 +1,132 @@
+## Evaluate if transitions are responsive to the climate using multinomiale regression
+## By Steve Vissault
+## Jeudi 10 février
+
+#Clean wd
+rm(list=ls())
+
+# Load library , data
+load("~/Documents/GitHub/STModel-Data-4S/out_files/transitions_r1.rdata")
+library("nnet")
+library("reshape2")
+
+#0. Filter data
+transitionData <- subset(transitionData,subset=annual_mean_temp<=10)
+
+#1. Scale variable
+scale_tp<- scale(transitionData$annual_mean_temp)
+transitionData$annual_mean_temp <- as.numeric(scale_tp)
+scale_pp <-  scale(transitionData$tot_annual_pp)
+transitionData$annual_pp <- as.numeric(scale_pp)
+
+#2. Add time interval
+transitionData$time <- transitionData$year2 - transitionData$year1
+
+#3. Remove imposible transitions
+transitionData$trobs <- paste0(transitionData$state1,transitionData$state2)
+transitionData <- subset(transitionData,trobs!="BT" & trobs!="TB")
+
+#4. Evaluate contribution of the coef: build all formula
+coefs <-c("annual_mean_temp" , "I(annual_mean_temp^2)" ,"I(annual_mean_temp^3)"  , "annual_pp" , "I(annual_pp^2)" ,"I(annual_pp^3)", "annual_mean_temp:annual_pp" , "time")
+
+contrib_form <- as.character()
+tested_coefs <- as.character()
+
+for(i in 1:length(coefs)){
+  temp_coefs <- coefs[-i]
+  tested_coefs  <- append(tested_coefs ,coefs[i])
+  form <- paste0("state2 ~ ",paste(temp_coefs,collapse="+"))
+  contrib_form <- append(contrib_form,form)
+}
+# Add full model
+full_formula <- paste0("state2 ~ ",paste(coefs,collapse="+"))
+null_formula <- "state2 ~ 1"
+
+# Create DF of formula
+formulas <- data.frame(tested=tested_coefs,formula=contrib_form,stringsAsFactors=FALSE)
+
+#5. Full Models
+T_multi_full <- multinom(full_formula,data=transitionData,subset= state1=='T')
+M_multi_full <- multinom(full_formula,data=transitionData,subset= state1=='M')
+R_multi_full <- multinom(full_formula,data=transitionData,subset= state1=='R')
+B_multi_full <- multinom(full_formula,data=transitionData,subset= state1=='B')
+
+#6. Null Models
+T_multi_null <- multinom(null_formula,data=transitionData,subset= state1=='T')
+M_multi_null <- multinom(null_formula,data=transitionData,subset= state1=='M')
+R_multi_null <- multinom(null_formula,data=transitionData,subset= state1=='R')
+B_multi_null <- multinom(null_formula,data=transitionData,subset= state1=='B')
+
+#7. Compute McFadden pseudo-R2
+T_Rs <- 1 - (T_multi_full$deviance / T_multi_null$deviance)
+M_Rs <- 1 - (M_multi_full$deviance / M_multi_null$deviance)
+R_Rs <- 1 - (R_multi_full$deviance / R_multi_null$deviance)
+B_Rs <- 1 - (B_multi_full$deviance / B_multi_null$deviance)
+
+#8. Compute contributions to the full model
+states <- c("T","M","R","B")
+ls_contrib <-list()
+
+for (state in 1:length(states)){
+  contrib <- as.numeric()
+  for(i in 1:nrow(formulas)){
+    multi <-multinom(formulas$formula[i],data=transitionData,subset= state1==states[state])
+    contrib <- append(contrib,(multi$AIC-get(paste0(states[state],'_multi_full'))$AIC))
+  }
+  ls_contrib[[state]] <- contrib
+}
+
+#9. Summarize information in table and export in latex
+contrib <- do.call("rbind",ls_contrib)
+colnames(contrib) <- formulas$tested
+rownames(contrib) <- states
+contrib <- data.frame(contrib, AIC=c(T_multi_full$AIC, M_multi_full$AIC, R_multi_full$AIC, B_multi_full$AIC), R2=c(T_Rs,M_Rs,R_Rs,B_Rs))
+
+# library("knitr")
+# kable(contrib,format="latex")
+
+#10. Evaluate probability along gradient
+library(RColorBrewer)
+library(colorRamps)
+pal <-colorRampPalette(rev(brewer.pal(11,"RdYlBu")))
+
+# get range for the specific transition
+#rg_tp <- range(transitionData[which(transitionData$state1==state),]$annual_mean_temp)
+#rg_pp <- range(transitionData[which(transitionData$state1==state),]$annual_pp)
+rg_tp <- range(transitionData$annual_mean_temp)
+rg_pp <- range(transitionData$annual_pp)
+
+states <- c("T","M","B","R")
+
+for (from in 1:length(states)){
+
+# STATE FROM
+state_from = states[from]
+
+# predict on scaled data
+pp <- seq(rg_pp[1], rg_pp[2], length.out = 200)
+tp <- seq(rg_tp[1], rg_tp[2], length.out = 200)
+prob = as.data.frame(predict(get(paste0(state_from,'_multi_full')), newdata = data.frame(expand.grid(annual_mean_temp = tp, annual_pp = pp),time=5 ), type = "probs"))
+
+# unscaled data
+pp <- pp  * attr(scale_pp,"scaled:scale") + attr(scale_pp,"scaled:center")
+tp <- tp * attr(scale_tp,"scaled:scale") + attr(scale_tp,"scaled:center")
+
+png(paste0("~/Desktop/From",state_from,".png"),res=300, width = 5, height = 5.5, units = 'in')
+# Set graphic with 4 panels
+par(mfrow=c(2,2),mar=c(2,2,1,1),oma=c(1,1,2,1))
+
+for(to in 1:length(states)){
+  # STATE TO
+  state_to <- states[to]
+  if(state_to %in% names(prob)){
+    image(x=pp, y=tp, z = t(matrix(prob[,which(names(prob)==state_to)], ncol = length(pp), nrow = length(tp))),xlab = "", ylab = "", col = pal(100),zlim=c(0,1))
+    contour(x=pp, y=tp, z = t(matrix(prob[,which(names(prob)==state_to)], ncol = length(pp), nrow = length(tp))),cex=0.5,add=TRUE)
+  title(main=paste("to",state_to),cex=0.8,font.main = 2)
+ }
+}
+mtext(paste("From",state_from),side=3,outer=TRUE,cex=1.2,font=2)
+mtext("Temperature",side=2,outer=TRUE,cex=1)
+mtext("Precipitation",side=1,outer=TRUE,cex=1)
+dev.off()
+}
---
+++ 2-LandMetrics/Turnover.r
@@ -0,0 +1,105 @@
+# Purpose:	Create map showing the rate of turnover (states transitons) for each time step.
+
+# Clean workspace
+rm(list=ls())
+
+# load library
+library('raster')
+library('doParallel')
+
+# Archive path
+arch <- Sys.getenv("ARCHIVE")
+
+# Get node addresses given by Torque and assigned to your task
+nodefile <- Sys.getenv("PBS_NODEFILE")
+hostlist <- read.table(nodefile,header=FALSE) # Node adresses
+
+# Get the number of cores by node
+nCores <- detectCores()
+
+# Open the cluster
+cl <- makeCluster(rep(c(as.character(hostlist$V1)),nCores),type='SOCK')
+# We replicated the addresses nCores times.
+registerDoParallel(cl)
+
+################################################
+# Some usefull functions
+stateToId <- function(state){
+	# Function stateToId
+	# Purpose: Convert state to id
+	state <- as.character(state)
+	state[state=="B"] <- 1
+	state[state=="T"] <- 2
+	state[state=="M"] <- 3
+	state[state=="R"] <- 4
+	state[is.na(state)] <- 0
+
+	return(as.numeric(state))
+}
+
+
+################################################
+# list all folders
+dirs <- list.dirs(paste0(arch,"/stm-out/stmodel-local"))
+dirs <- grep("rep_",dirs,value=TRUE)
+
+# list of files in the folder
+files <- paste0(c('0000',as.character(seq(2000,2095,5))),"_land_rs.robj")
+
+# This one can be parralelized
+MtoT <- foreach(dir=1:length(dirs),.packages='raster') %dopar% {
+  ls_over <- list()
+  for (file in 2:length(files)){
+  	# for each time step in the folder
+  	load(paste(dirs[dir],files[file],sep="/"))
+    rs[rs==0] <- NA
+    rst1 <- rs
+    load(paste(dirs[dir],files[file-1],sep="/"))
+    rs[rs==0] <- NA
+    rst0 <- rs
+
+    ls_over[[file-1]] <- overlay(rst1, rst0, fun = function(t1,t0) { ifelse( t1 == stateToId('T') &  t0 == stateToId('M') , 1, 0) })
+  }
+  sum(stack(ls_over))
+}
+
+MtoT <- mean(stack(MtoT))
+
+BtoM <- foreach(dir=1:length(dirs),.packages='raster') %dopar% {
+  ls_over <- list()
+  for (file in 2:length(files)){
+  	# for each time step in the folder
+  	load(paste(dirs[dir],files[file],sep="/"))
+    rs[rs==0] <- NA
+    rst1 <- rs
+    load(paste(dirs[dir],files[file-1],sep="/"))
+    rs[rs==0] <- NA
+    rst0 <- rs
+
+    ls_over[[file-1]] <- overlay(rst1, rst0, fun = function(t1,t0) { ifelse( t1 == stateToId('M') &  t0 == stateToId('B') , 1, 0) })
+  }
+  sum(stack(ls_over))
+}
+
+BtoM <- mean(stack(BtoM))
+
+BTMtoR <- foreach(dir=1:length(dirs),.packages='raster') %dopar% {
+  ls_over <- list()
+  for (file in 2:length(files)){
+  	# for each time step in the folder
+  	load(paste(dirs[dir],files[file],sep="/"))
+    rs[rs==0] <- NA
+    rst1 <- rs
+    load(paste(dirs[dir],files[file-1],sep="/"))
+    rs[rs==0] <- NA
+    rst0 <- rs
+
+    ls_over[[file-1]] <- overlay(rst1, rst0, fun = function(t1,t0) { ifelse( t1 == stateToId('R')  &  (t0 == stateToId('T') | t0 == stateToId('B') | t0 == stateToId('M')), 1, 0) })
+  }
+  sum(stack(ls_over))
+}
+BTMtoR <- mean(stack(BTMtoR))
+
+# Save all object
+
+stopCluster(cl)
